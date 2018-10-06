49248473219a4a65afc8ce26f4817eb0ff7257ec
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-10-07 00:09:35 +0600

    Made app and pages into a module

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from .views import LandingPage
 
-app_name = 'pages'
+app_name = 'app'
 
 urlpatterns = [
     url(r'^$', LandingPage.as_view(), name='landing-page')
